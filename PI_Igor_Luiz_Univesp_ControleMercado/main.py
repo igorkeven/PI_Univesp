f0ff01440a50ef06fe5a921b5680dc167375978b
igorkeven@hotmail.com <igorkeven@hotmail.com>
2023-03-26 12:44:45 -0300

    atualização 3

--- PI_Igor_Luiz_Univesp_ControleMercado/main.py
+++ PI_Igor_Luiz_Univesp_ControleMercado/main.py
@@ -5,12 +5,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
+# acesso a pagina inicial
 @app.route("/")
 def index():
     return render_template("html/home.html")
 
 
+# acesso a pagina produtos
+@app.route("/produtos")
+def produtos():
+    return render_template('html/produtos.html')
+
+
+# acesso a pagina de login do cliente para liberar as compras
+@app.route("/login")
+def login():
+    return render_template('html/login.html')
+
+# acesso a pagina de login do artesão
+@app.route("/loginArtesao")
+def loginArtesao():
+    return render_template('html/loginArtesao.html')
+
+# acesso a pagina do artesão onde ele fara upload dos artesanatos
+@app.route("/artesao")
+def artesao():
+    return render_template('html/artesao.html')
 
 
 
